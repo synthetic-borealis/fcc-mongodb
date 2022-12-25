baae3c9c94b8c5f9ea40acb31089191b295d9875
synthetic-borealis <snip.coloured@gmail.com>
2022-12-25 21:33:36 +0200

    step 7: findPersonById

--- myApp.js
+++ myApp.js
@@ -49,7 +49,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, function(err, personFound) {
+    if (err) return console.error(err);
+    done(null, personFound)
+  });
 };
 
 const findEditThenSave = (personId, done) => {
