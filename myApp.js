5ad7c0389090811427cea20ed234ea1687d9e7d7
synthetic-borealis <snip.coloured@gmail.com>
2022-12-26 00:11:48 +0200

    step 9: findOneThenUpdate

--- myApp.js
+++ myApp.js
@@ -72,7 +72,10 @@ const findEditThenSave = (personId, done) => {
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
 
-  done(null /*, data*/);
+  Person.findOneAndUpdate({ name: personName }, { age: ageToSet }, function(err, updatedPerson) {
+    if (err) return console.error(err);
+    done(null, updatedPerson)
+  });
 };
 
 const removeById = (personId, done) => {
