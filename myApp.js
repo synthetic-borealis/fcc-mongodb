d39169e12b50a59744b27a6dbe4f1d1da0f47300
synthetic-borealis <snip.coloured@gmail.com>
2022-12-26 10:26:45 +0200

    fix

--- myApp.js
+++ myApp.js
@@ -100,7 +100,7 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  Person.find({ favoriteFoods: foodToSearch })
+  Person.find({ favoriteFoods: [foodToSearch] })
     .sort({ name: -1 })
     .limit(2)
     .select({ age: 0 })
