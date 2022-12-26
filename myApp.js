53e4ec765279bdd176090004815a03355bdaf90f
synthetic-borealis <snip.coloured@gmail.com>
2022-12-26 10:24:43 +0200

    step 12: queryChain

--- myApp.js
+++ myApp.js
@@ -100,7 +100,14 @@ const removeManyPeople = (done) => {
 const queryChain = (done) => {
   const foodToSearch = "burrito";
 
-  done(null /*, data*/);
+  Person.find({ favoriteFoods: foodToSearch })
+    .sort({ name: -1 })
+    .limit(2)
+    .select({ age: 0 })
+    .exec((err, peopleFound) => {
+      if (err) return console.log(err);
+      done(null, peopleFound);
+    });
 };
 
 /** **Well Done !!**
